5b5d418b6540404fea1b7183cfdd8e436846d08f
Duncan McGreggor <oubiwann@gmail.com>
2018-03-17 13:31:13 -0500

    Use curl instead.

--- kafka/scripts/common.sh
+++ kafka/scripts/common.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-SCALA_VERSION=2.12
-KAFKA_VERSION=1.0.1
+SCALA_VERS=2.12
+KAFKA_VERS=1.0.1
+SCALA_KAFKA_VERS=${SCALA_VERS}-${KAFKA_VERS}
 INSTALL_OPTS="-y --no-install-recommends"
-PACKAGES="zookeeper wget supervisor dnsutils"
+PACKAGES="curl zookeeper supervisor dnsutils"
--- kafka/scripts/install.sh
+++ kafka/scripts/install.sh
@@ -2,11 +2,12 @@
 
 . /build/common.sh
 
-wget \
--q \
-http://apache.mirrors.spacedump.net/kafka/"$KAFKA_VERSION"/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz \
--O /tmp/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz
+HTTP_ENDPOINT=http://apache.mirrors.spacedump.net/kafka
 
-tar xfz /tmp/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz -C /opt && \
+curl \
+$HTTP_ENDPOINT/$KAFKA_VERS/kafka_$SCALA_KAFKA_VERS.tgz \
+-o /tmp/kafka_$SCALA_KAFKA_VERS.tgz
 
-rm /tmp/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz
+tar xfz /tmp/kafka_$SCALA_KAFKA_VERS.tgz -C /opt && \
+
+rm /tmp/kafka_$SCALA_KAFKA_VERS.tgz
